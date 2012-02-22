1fd494d5711831ef61832f70e7dfec471eebe485
Martin Mägar <martin.magar@elion.ee>
2012-02-22 09:06:31 +0200

    Check password match also when password field changes in addition to just when the passwordRepeat field changes.

--- javascript/validations/validateRegForm.js
+++ javascript/validations/validateRegForm.js
@@ -1,4 +1,4 @@
-var validateUsername = function() {
+function validateUsername() {
 	var username = $(this);
 	var errorMsg = username.next();
 	var controlGroup = username.parent().parent();
@@ -6,46 +6,54 @@ var validateUsername = function() {
   		controlGroup.addClass('error').removeClass('success');
   		errorMsg.text("Username too short.");
   		errorMsg.show();
+  		return false;
   	} else {
   		controlGroup.removeClass('error').addClass('success');
   		errorMsg.hide();
+  		return true;
   	}
 };
 
-var validatePasswordRepeat = function() {
-	var passwordRep = $(this);
+function validatePasswordRepeat() {
+	var passwordRep = $('#passwordRepeat');
 	var password = $('#password');
 	var errorMsg = passwordRep.next();
 	var controlGroup = passwordRep.parent().parent();
   	if(passwordRep.val() == password.val()){
   		controlGroup.removeClass('error').addClass('success');
   		errorMsg.hide();
+  		return true;
   	} else {
   		controlGroup.addClass('error').removeClass('success');
   		errorMsg.text("Passwords do not match!");
   		errorMsg.show();
+  		return false;
   	}
 };
 
-var validatePassword = function() {
-	var password = $(this);
+function validatePassword() {
+	var password = $('#password');
 	var errorMsg = password.next();
 	var controlGroup = password.parent().parent();
+	validatePasswordRepeat();
   	if(password.val().length < 4){
   		controlGroup.addClass('error').removeClass('success').removeClass('warning');
   		errorMsg.text("Password too short!");
   		errorMsg.show();
+  		return false;
   	} else if(password.val().length < 8){
   		controlGroup.addClass('warning').removeClass('error').removeClass('success');
   		errorMsg.text("Password weak.");
   		errorMsg.show();
+  		return false;
   	} else {
   		controlGroup.addClass('success').removeClass('error').removeClass('warning');
   		errorMsg.hide();
+  		return true;
   	}
 };
 
-var validateEmail = function() {
+function validateEmail() {
 	var email = $(this);
 	var errorMsg = email.next();
 	var controlGroup = email.parent().parent();
@@ -53,10 +61,16 @@ var validateEmail = function() {
   		controlGroup.addClass('error').removeClass('success');
   		errorMsg.text("Email should contain @ sign.");
   		errorMsg.show();
+  		return false;
   	} else {
   		controlGroup.removeClass('error').addClass('success');
   		errorMsg.hide();
+  		return true;
   	}
+};
+
+var validateRegForm = function(){
+	return false;
 };
 
 $('#username').keyup(validateUsername);
