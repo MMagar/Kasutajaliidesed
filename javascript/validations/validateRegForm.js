e66a6a66b147ca71b5ccd29f8cdf231ce4f9c08e
Martin Mägar <martin.magar@elion.ee>
2012-02-22 09:44:53 +0200

    Register button now checks the entire form.

--- javascript/validations/validateRegForm.js
+++ javascript/validations/validateRegForm.js
@@ -1,10 +1,10 @@
 function validateUsername() {
-	var username = $(this);
+	var username = $('#username');
 	var errorMsg = username.next();
 	var controlGroup = username.parent().parent();
   	if(username.val().length < 4){
   		controlGroup.addClass('error').removeClass('success');
-  		errorMsg.text("Username too short.");
+  		errorMsg.text("Username too short!");
   		errorMsg.show();
   		return false;
   	} else {
@@ -19,7 +19,12 @@ function validatePasswordRepeat() {
 	var password = $('#password');
 	var errorMsg = passwordRep.next();
 	var controlGroup = passwordRep.parent().parent();
-  	if(passwordRep.val() == password.val()){
+	var passwordGroup = controlGroup.prev();
+  	if(passwordGroup.hasClass('error')){
+  		controlGroup.removeClass('success').addClass('error');
+  		errorMsg.hide();
+  		return false;
+  	} else if(passwordRep.val() == password.val()){
   		controlGroup.removeClass('error').addClass('success');
   		errorMsg.hide();
   		return true;
@@ -35,26 +40,28 @@ function validatePassword() {
 	var password = $('#password');
 	var errorMsg = password.next();
 	var controlGroup = password.parent().parent();
-	validatePasswordRepeat();
   	if(password.val().length < 4){
   		controlGroup.addClass('error').removeClass('success').removeClass('warning');
+  		validatePasswordRepeat();
   		errorMsg.text("Password too short!");
   		errorMsg.show();
   		return false;
   	} else if(password.val().length < 8){
   		controlGroup.addClass('warning').removeClass('error').removeClass('success');
+  		validatePasswordRepeat();
   		errorMsg.text("Password weak.");
   		errorMsg.show();
   		return false;
   	} else {
   		controlGroup.addClass('success').removeClass('error').removeClass('warning');
+  		validatePasswordRepeat();
   		errorMsg.hide();
   		return true;
   	}
 };
 
 function validateEmail() {
-	var email = $(this);
+	var email = $('#email');
 	var errorMsg = email.next();
 	var controlGroup = email.parent().parent();
   	if(email.val().indexOf("@") == -1){
@@ -70,10 +77,20 @@ function validateEmail() {
 };
 
 var validateRegForm = function(){
-	return false;
+	var wasError = false;
+	if(!validateUsername())
+		wasError = true;
+	if(!validatePassword())
+		wasError = true;
+	if(!validatePasswordRepeat())
+		wasError = true;
+	if(!validateEmail())
+		wasError = true;
+	return wasError;
 };
 
 $('#username').keyup(validateUsername);
 $('#passwordRepeat').keyup(validatePasswordRepeat);
 $('#password').keyup(validatePassword);
 $('#email').keyup(validateEmail);
+$('#registerButton').click(validateRegForm);
