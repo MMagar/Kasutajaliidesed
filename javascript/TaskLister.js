05026ff476198e9bc8bdfe33562769c760b0b039
Martin Mägar <martin.magar@elion.ee>
2012-03-10 01:54:46 +0200

    Moved TaskLister.js to a separate file.

---
+++ javascript/TaskLister.js
@@ -0,0 +1,32 @@
+var TaskLister = {
+	init: function (config) {
+		this.config = config;
+		var taskItemSource = $("#taskItem-template").html();
+		this.taskItemTemplate = Handlebars.compile(taskItemSource); 
+		this.fetchAllTasks();
+		this.drawAllTasks();
+	},
+
+	fetchAllTasks: function(){
+		this.tasksData = {"taskItems":[
+					{"header": "Clean the room",
+					"urgency": "2",
+					"importance": "3",
+					"deadline": "20:00 2012/08/24",
+					"description": "Mingi pikem tekst. Mingi pikem tekst. Mingi pikem tekst.Mingi pikem tekst.",
+					"status": "started"},
+					
+					{"header": "Fix the car",
+					"urgency": "4",
+					"importance": "2",
+					"deadline": "14:00 2020/08/14",
+					"description": "Auto ikka katki",
+					"status": "started"}]	
+				};
+	},
+
+	drawAllTasks: function() {
+		var tasksHTML = taskItemTemplate(tasksData);
+		$('.taskList').append(result);
+	}
+};
